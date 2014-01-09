7000708fbe7175965151f60957a75d70d6b63686
Aman Gupta <aman@tmm1.net>
2014-01-08 22:06:32 -0800

    avoid building exts on older rubies

--- ext/oobgc/extconf.rb
+++ ext/oobgc/extconf.rb
@@ -1,5 +1,12 @@
 require 'mkmf'
 have_func('rb_gc_stat')
 have_func('rb_gc_latest_gc_info')
-have_const('RUBY_INTERNAL_EVENT_GC_END_MARK')
-create_makefile('gctools/oobgc')
+gc_event = have_const('RUBY_INTERNAL_EVENT_GC_END_MARK')
+
+if gc_event
+  create_makefile('gctools/oobgc')
+else
+  File.open('Makefile', 'w') do |f|
+    f.puts "install:\n\t\n"
+  end
+end
